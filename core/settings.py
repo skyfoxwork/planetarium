7406094be9d24f3ecdc22773497afe1921253a45
roman <skyfoxwork@gmail.com>
2024-12-21 17:02:55 +0200

    add planetarium, user, rest_framework app names to INSTALLED_APPS to core/settings.py module

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # third party app
+    "rest_framework",
+
+    # local app
+    "planetarium",
+    "user"
 ]
 
 MIDDLEWARE = [
