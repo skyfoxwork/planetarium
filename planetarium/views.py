a921ca7a7c21843bf40ea3c2db84ef0d29f28d1d
roman <skyfoxwork@gmail.com>
2024-12-22 14:07:34 +0200

    add ShowThemeViewSet to planetarium/views.py

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,9 +1,21 @@
 from rest_framework import viewsets
 
-from planetarium.models import AstronomyShow
-from planetarium.serializers import AstronomyShowSerializer
+from planetarium.models import (
+    AstronomyShow,
+    ShowTheme,
+)
+
+from planetarium.serializers import (
+    AstronomyShowSerializer,
+    ShowThemeSerializer,
+)
 
 
-class AstronomyShowView(viewsets.ModelViewSet):
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.prefetch_related("theme")
     serializer_class = AstronomyShowSerializer
