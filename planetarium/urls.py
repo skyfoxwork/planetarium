581f09190e6f30b6c84d38b9fa35bf735770a34b
roman <skyfoxwork@gmail.com>
2024-12-22 14:23:41 +0200

    add endpoints planetarium-domes to planetarium/urls.py

--- planetarium/urls.py
+++ planetarium/urls.py
@@ -5,12 +5,14 @@ from rest_framework import routers
 from planetarium.views import (
     AstronomyShowViewSet,
     ShowThemeViewSet,
+    PlanetariumDomeViewSet,
 )
 
 
 router = routers.DefaultRouter()
 
-router.register("themes", ShowThemeViewSet),
+router.register("themes", ShowThemeViewSet)
+router.register("planetarium-domes", PlanetariumDomeViewSet)
 router.register("astronomy-shows", AstronomyShowViewSet)
 
 urlpatterns = [path("", include(router.urls))]
