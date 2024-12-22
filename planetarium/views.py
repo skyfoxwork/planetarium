663708dcc2d99784051b8e83d70c2965810ee272
roman <skyfoxwork@gmail.com>
2024-12-22 16:36:14 +0200

    add ShowSessionViewSet and ReservationViewSet to planetarium/views.py

--- planetarium/views.py
+++ planetarium/views.py
@@ -2,13 +2,18 @@ from rest_framework import viewsets
 
 from planetarium.models import (
     AstronomyShow,
-    ShowTheme, PlanetariumDome,
+    ShowTheme,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
 )
 
 from planetarium.serializers import (
     AstronomyShowSerializer,
     ShowThemeSerializer,
     PlanetariumDomeSerializer,
+    ShowSessionSerializer,
+    ReservationSerializer,
 )
 
 
@@ -25,3 +30,15 @@ class PlanetariumDomeViewSet(viewsets.ModelViewSet):
 class AstronomyShowViewSet(viewsets.ModelViewSet):
     queryset = AstronomyShow.objects.prefetch_related("theme")
     serializer_class = AstronomyShowSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.select_related(
+        "astronomy_show", "planetarium_dome"
+    )
+    serializer_class = ShowSessionSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
