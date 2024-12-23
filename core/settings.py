04c4fcf919a7589556f0583038c5ca17fe206733
roman <skyfoxwork@gmail.com>
2024-12-23 19:16:25 +0200

    add throttling in REST_FRAMEWORK to core/settings.py

--- core/settings.py
+++ core/settings.py
@@ -145,6 +145,11 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "5/day", "user": "30/day"},
 }
 
 SIMPLE_JWT = {
