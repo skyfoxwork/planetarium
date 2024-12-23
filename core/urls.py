37a110c096a219443d550eb94850bfca77f89521
roman <skyfoxwork@gmail.com>
2024-12-23 19:11:46 +0200

    add endpoints api/schema/, doc/swagger/ and api/doc/redoc/ for drf_spectacular to core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,25 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]  + debug_toolbar_urls()
