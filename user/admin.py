e15af3bf1466be4b5dd1fe16e63a043c6a62ad77
roman <skyfoxwork@gmail.com>
2024-12-24 11:22:17 +0200

    refactor UserAdmin registration in admin panel to user/admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
-from user.models import User
+from .models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "first_name", "last_name", "email", "is_staff")
-    search_fields = ("username", "first_name", "last_name", "email")
+class UserAdmin(DjangoUserAdmin):
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
