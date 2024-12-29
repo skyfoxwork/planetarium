44756ba098ebbc95e2fb7c069309a8de710d53a0
roman <skyfoxwork@gmail.com>
2024-12-29 19:33:12 +0200

    fix imports more than two items

--- planetarium/views.py
+++ planetarium/views.py
@@ -3,7 +3,11 @@ from datetime import datetime
 from django.db.models import F, Count
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import OpenApiParameter, extend_schema
-from rest_framework import viewsets, mixins, status
+from rest_framework import (
+    viewsets,
+    mixins,
+    status
+)
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
